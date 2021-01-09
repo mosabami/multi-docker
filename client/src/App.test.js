384cad37881ff5a2c76a12b31d79a72fadba3e83
mosabami <ayobamiaayodeji@gmail.com>
2021-01-09 01:58:39 -0500

    change travis yml and app.text

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,5 +4,5 @@ import App from './App';
 test('renders learn react link', () => {
   // render(<App />);
   // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument(); // test wil try and render the content of Fib which requires access to the database servers which are not live yet. Disable to avoid problems
+  // expect(linkElement).toBeInTheDocument(); // test wil try and render the content of Fib which requires access to the api servers which are not live yet. Disable to avoid problems
 });
